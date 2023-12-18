9476427d57368f5ec7cdeed2eb2844291c696884
neuralproduction <neuralproductionapps@gmail.com>
2023-12-18 10:56:46 +0530

    new version updte

--- WebIOTApplication/UI/NeuralIOT/src/app/app.module.ts
+++ WebIOTApplication/UI/NeuralIOT/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { SignupComponent } from './Components/signup/signup.component';
 import { MqttDashboardComponent } from './Components/mqtt-dashboard/mqtt-dashboard.component';
 import { TopNavBarComponent } from './Components/top-nav-bar/top-nav-bar.component';
 import { HomeComponent } from './Components/home/home.component';
-import { NgbModule, NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -28,7 +27,6 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbCollapse,
     HttpClientModule,
     MqttModule.forRoot(mqttServiceOptions),
   ],
